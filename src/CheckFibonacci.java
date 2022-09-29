66054d760a92547879948f9dcae22da9257bc89d
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-09-28 23:32:13 -0700

    annotated it, also there will be another case when they give any particular sequence of fibo we can modify the conditions accordingly

--- src/CheckFibonacci.java
+++ src/CheckFibonacci.java
@@ -1,4 +1,5 @@
 public class CheckFibonacci {
+    //array in parameter, and if it contains any fibo seq then return true
     public static boolean checkFibbo(int [] arr){
         for(int i=0; i<arr.length; i++){
             if(arr[i] +arr[i+1] == arr[i+2]){
@@ -8,7 +9,7 @@ public class CheckFibonacci {
     }
 
     public static void main(String[] args) {
-        int []arr = { 2,33,52,24,4,28};
+        int []arr = {2,33,52,24,4,28};
         System.out.println(checkFibbo(arr));
     }
 }
